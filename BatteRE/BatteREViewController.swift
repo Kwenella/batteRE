f47417073fa897fb0a6728dd77ba94e207d0a83e
K_R <K_R@K-Rs-MacBook-Pro.local>
2017-11-05 14:04:12 +0200

    first commit:
    z
    ;
    :

---
+++ BatteRE/BatteREViewController.swift
@@ -0,0 +1,18 @@
+//
+//  BatteREViewController.swift
+//  BatteRE
+//
+//  Created by K_R on 11/5/17.
+//  Copyright © 2017 K_R. All rights reserved.
+//
+
+import Cocoa
+
+class BatteREViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
