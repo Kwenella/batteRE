8a901ca3049b7760c3d8451b66d9bd40081ed443
K_R <K_R@K-Rs-MacBook-Pro.local>
2017-11-05 14:05:41 +0200

    first commit

--- BatteRE/BatteREViewController.swift
+++ BatteRE/BatteREViewController.swift
@@ -12,7 +12,27 @@ class BatteREViewController: NSViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do view setup here.
+        
+        //self.view.wantsLayer = true
+        //let image = NSImage(named: NSImage.Name(rawValue: "backgound.png"))
+        //self.view.layer!.contents = image
     }
     
+}
+
+extension BatteREViewController {
+    // Storyboard instantiation
+    static func freshController() -> BatteREViewController {
+        //1. Get a reference to Main.storyboard
+        let storyboard = NSStoryboard(name: NSStoryboard.Name(rawValue: "Main"), bundle: nil)
+        //2. Create a Scene identifier that matches the one you set just before
+        let identifier = NSStoryboard.SceneIdentifier(rawValue: "BatteREViewController")
+        //3. Instantiate BatteREViewController and return it
+        guard let viewcontroller = storyboard.instantiateController(withIdentifier: identifier) as? BatteREViewController else {
+            fatalError("Cant find BatteREViewController? - Check Main.storyboard")
+        }
+        return viewcontroller
+    }
 }
