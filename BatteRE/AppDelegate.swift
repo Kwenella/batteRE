d7af888d9de694892e40c63048834e0de2ba1203
K_R <K_R@K-Rs-MacBook-Pro.local>
2017-11-05 09:35:04 +0200

    Initial Commit

---
+++ BatteRE/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  BatteRE
+//
+//  Created by K_R on 11/5/17.
+//  Copyright © 2017 K_R. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ BatteRE/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BatteRE
+//
+//  Created by K_R on 11/5/17.
+//  Copyright © 2017 K_R. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
